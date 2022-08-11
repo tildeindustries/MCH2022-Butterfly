2c2ffc2f7b2ab90025a95f5637097e0b1ae334ab
Kuristian <Kuristian@users.noreply.github.com>
2022-08-11 23:32:17 +0100

    Updated the badge code to run the first demo

--- Badge App/main/main.c
+++ Badge App/main/main.c
@@ -2,10 +2,40 @@
 #include "main.h"
 #include "rp2040.h"
 
+#define Colour1 0x00058240
+#define Colour2 0x0087C159
+#define Colour3 0x00ADCACB
+#define Colour4 0x00FEE3A2
+#define Colour5 0x00F3C301
+#define White   0x00FFFFFF
+
 static pax_buf_t buf;
 xQueueHandle buttonQueue;
 
 static const char *TAG = "Tilde addon";
+
+const static uint8_t GammaTable[] = {
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+  2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+  90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+void delay_(unsigned long time_delay)
+{
+    vTaskDelay(pdMS_TO_TICKS(time_delay));
+}
 
 // Updates the screen with the latest buffer.
 void disp_flush() {
@@ -21,89 +51,48 @@ void exit_to_launcher() {
 void sao_demo() {
     RP2040* rp2040 = get_rp2040();
     
-    for (uint8_t j = 0; j < 127; j++) {
-        for (uint8_t i = 0; i < 5; i++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 16);
-        }
+    void DisplayCorrectedHexColour(int Position, long unsigned int Colour, long unsigned int brightness)
+    {
+        long unsigned int RemappedColour = 0;
+        RemappedColour = RemappedColour + (GammaTable[(((Colour & 0x00FF0000) >> 16) * brightness / 255)] << 16); // Red
+        RemappedColour = RemappedColour + (GammaTable[(((Colour & 0x0000FF00) >> 8) * brightness / 255)] << 24); // Green
+        RemappedColour = RemappedColour + (GammaTable[((Colour & 0x000000FF) * brightness / 255)] << 8); // Blue
+        rp2040_set_ws2812_data(rp2040, Position, RemappedColour);
         rp2040_ws2812_trigger(rp2040);
-        vTaskDelay(pdMS_TO_TICKS(5));
     }
-    for (uint8_t j = 127; j > 0; j--) {
-        for (uint8_t i = 0; i < 5; i++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 16);
+
+    for (int i = 0; i < 180; i++)
+    {
+        for (int j = 0; j < NUMPIXELS; j++) {
+            DisplayCorrectedHexColour(j, White, i);
         }
-        rp2040_ws2812_trigger(rp2040);
-        vTaskDelay(pdMS_TO_TICKS(5));
+        delay_(3 + i / 16);
     }
-    for (uint8_t j = 0; j < 127; j++) {
-        for (uint8_t i = 0; i < 5; i++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 24);
+    delay_(500);
+    for (int i = 180; i < 255; i++)
+    {
+        for (int j = 0; j < NUMPIXELS; j++) {
+            DisplayCorrectedHexColour(j, White, i);
         }
-        rp2040_ws2812_trigger(rp2040);
-        vTaskDelay(pdMS_TO_TICKS(5));
+        delay_(2);
     }
-    for (uint8_t j = 127; j > 0; j--) {
-        for (uint8_t i = 0; i < 5; i++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 24);
+    for (int i = 255; i > 0; i--)
+    {
+        for (int j = 0; j < NUMPIXELS; j++) {
+            DisplayCorrectedHexColour(j, White, i);
         }
-        rp2040_ws2812_trigger(rp2040);
-        vTaskDelay(pdMS_TO_TICKS(5));
+        delay_(2);
     }
-    for (uint8_t j = 0; j < 127; j++) {
-        for (uint8_t i = 0; i < 5; i++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 8);
-        }
-        rp2040_ws2812_trigger(rp2040);
-        vTaskDelay(pdMS_TO_TICKS(5));
-    }
-    for (uint8_t j = 127; j > 0; j--) {
-        for (uint8_t i = 0; i < 5; i++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 8);
-        }
-        rp2040_ws2812_trigger(rp2040);
-        vTaskDelay(pdMS_TO_TICKS(5));
-    }
-    for (uint8_t i = 0; i < 5; i++) {
-        for (uint8_t j = 0; j < 127; j++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 24);
-            rp2040_ws2812_trigger(rp2040);
-            vTaskDelay(pdMS_TO_TICKS(5));
-        }
-    }
-    for (uint8_t i = 0; i < 5; i++) {
-        for (uint8_t j = 127; j > 0; j--) {
-            rp2040_set_ws2812_data(rp2040, i, j << 24);
-            rp2040_ws2812_trigger(rp2040);
-            vTaskDelay(pdMS_TO_TICKS(5));
-        }
-    }
-    for (uint8_t i = 0; i < 5; i++) {
-        for (uint8_t j = 0; j < 127; j++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 16);
-            rp2040_ws2812_trigger(rp2040);
-            vTaskDelay(pdMS_TO_TICKS(5));
-        }
-    }
-    for (uint8_t i = 0; i < 5; i++) {
-        for (uint8_t j = 127; j > 0; j--) {
-            rp2040_set_ws2812_data(rp2040, i, j << 16);
-            rp2040_ws2812_trigger(rp2040);
-            vTaskDelay(pdMS_TO_TICKS(5));
-        }
-    }
-    for (uint8_t i = 0; i < 5; i++) {
-        for (uint8_t j = 0; j < 127; j++) {
-            rp2040_set_ws2812_data(rp2040, i, j << 8);
-            rp2040_ws2812_trigger(rp2040);
-            vTaskDelay(pdMS_TO_TICKS(5));
-        }
-    }
-    for (uint8_t i = 0; i < 5; i++) {
-        for (uint8_t j = 127; j > 0; j--) {
-            rp2040_set_ws2812_data(rp2040, i, j << 8);
-            rp2040_ws2812_trigger(rp2040);
-            vTaskDelay(pdMS_TO_TICKS(5));
-        }
+    delay_(500);
+
+    for (int i = 0; i < 255; i++)
+    {
+        DisplayCorrectedHexColour(0, Colour1, i);
+        DisplayCorrectedHexColour(1, Colour2, i);
+        DisplayCorrectedHexColour(2, Colour3, i);
+        DisplayCorrectedHexColour(3, Colour4, i);
+        DisplayCorrectedHexColour(4, Colour5, i);
+        delay_(3);
     }
 }
 
--- Misc/Demo_butterfly_animation_11-08-2022/Demo_butterfly_animation_11-08-2022.ino
+++ Misc/Demo_butterfly_animation_11-08-2022/Demo_butterfly_animation_11-08-2022.ino
@@ -1,22 +1,51 @@
 #include <Adafruit_NeoPixel.h>
-#include "GammaCorrectionLib.h"
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
 #define PIN         12
-#define NUMPIXELS   5
 
 #define Colour1 0x00058240
 #define Colour2 0x0087C159
 #define Colour3 0x00ADCACB
 #define Colour4 0x00FEE3A2
 #define Colour5 0x00F3C301
+#define White   0x00FFFFFF
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int value = 0;
+Adafruit_NeoPixel pixels(5, PIN, NEO_GRB + NEO_KHZ800);
 
-void DisplayCorrectedHexColour (int Position, long unsigned int Colour);
+const static uint8_t GammaTable[] = {
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+  2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+  5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+  90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
+  115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
+  144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
+  177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
+  215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
+};
+
+void DisplayCorrectedHexColour (int Position, long unsigned int Colour, long unsigned int brightness)
+{
+  pixels.setPixelColor(Position, pixels.Color(
+                         GammaTable[(((Colour & 0x00FF0000) >> 16) * brightness / 255)],
+                         GammaTable[((Colour & 0x0000FF00) >> 8)* brightness / 255],
+                         GammaTable[(Colour & 0x000000FF)* brightness / 255]));
+  pixels.show();
+}
+
+void delay_ (unsigned long time_delay)
+{
+  delay(time_delay);
+}
 
 void setup() {
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -31,53 +60,36 @@ void loop() {
   pixels.clear();
   for (int i = 0; i < 180; i++)
   {
-    value = GammaCorrection::Correct8(i);
-    for (int j = 0; j < NUMPIXELS; j++) {
-      pixels.setPixelColor(j, pixels.Color(value, value, value));
+    for (int j = 0; j < 5; j++) {
+      DisplayCorrectedHexColour (j, White, i);
     }
-    pixels.show();
-    delay(3+i/16);
+    delay_(3 + i / 16);
   }
-  delay(500);
-    for (int i = 180; i < 255; i++)
+  delay_(500);
+  for (int i = 180; i < 255; i++)
   {
-    value = GammaCorrection::Correct8(i);
-    for (int j = 0; j < NUMPIXELS; j++) {
-      pixels.setPixelColor(j, pixels.Color(value, value, value));
+    for (int j = 0; j < 5; j++) {
+      DisplayCorrectedHexColour (j, White, i);
     }
-    pixels.show();
-    delay(2);
+    delay_(2);
   }
   for (int i = 255; i > 0; i--)
   {
-    value = GammaCorrection::Correct8(i);
-    for (int j = 0; j < NUMPIXELS; j++) {
-      pixels.setPixelColor(j, pixels.Color(value, value, value));
+    for (int j = 0; j < 5; j++) {
+      DisplayCorrectedHexColour (j, White, i);
     }
-    pixels.show();
-    delay(2);
+    delay_(2);
   }
-  delay(500);
+  delay_(500);
 
   for (int i = 0; i < 255; i++)
   {
-    //pixels.setPixelColor(0, pixels.Color(GammaCorrection::Correct8((Colour1 & 0x00FF0000) >> 16), GammaCorrection::Correct8((Colour1 & 0x0000FF00) >> 8), GammaCorrection::Correct8(Colour1 & 0x000000FF)));
     DisplayCorrectedHexColour (0, Colour1, i);
     DisplayCorrectedHexColour (1, Colour2, i);
     DisplayCorrectedHexColour (2, Colour3, i);
     DisplayCorrectedHexColour (3, Colour4, i);
     DisplayCorrectedHexColour (4, Colour5, i);
-
-    pixels.show();
-    delay(3);
- }   
-while(1);
-}
-
-void DisplayCorrectedHexColour (int Position, long unsigned int Colour, long unsigned int brightness)
-{
-   pixels.setPixelColor(Position, pixels.Color(
-   GammaCorrection::Correct8(((Colour & 0x00FF0000) >> 16) * brightness/255), 
-   GammaCorrection::Correct8(((Colour & 0x0000FF00) >> 8)* brightness/255), 
-   GammaCorrection::Correct8((Colour & 0x000000FF)* brightness/255)));
+    delay_(3);
+  }
+  while (1);
 }
